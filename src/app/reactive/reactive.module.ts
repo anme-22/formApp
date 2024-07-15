94f6ff52912922500bd416a5e5f35fd0d216faec
anme-22 <angeldavidmendez2012@gmail.com>
2024-07-15 16:49:13 -0600

    reseting form

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { ReactiveRoutingModule } from './reactive-routing.module';
+
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
@@ -12,6 +13,6 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
     DynamicPageComponent,
     SwitchesPageComponent,
   ],
-  imports: [CommonModule, ReactiveRoutingModule],
+  imports: [CommonModule, ReactiveFormsModule, ReactiveRoutingModule],
 })
 export class ReactiveModule {}
